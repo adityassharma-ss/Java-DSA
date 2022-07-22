28236892359b702d5fd0a8daa43d8a98d3579a14
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-07-23 01:39:38 +0530

    Add files via upload

--- Arrays/SubSetsArray.java
+++ Arrays/SubSetsArray.java
@@ -1,6 +1,6 @@
 import java.io.*;
-import java.util.*;
 
+import java.util.*;
 public class SubSetsArray
 {
   public static void main (String[]args) throws Exception
@@ -10,32 +10,33 @@ public class SubSetsArray
     int[] arr = new int[n];
     for (int i = 0; i < arr.length; i++)
     {
-        arr[i] = scn.nextInt ();
+      arr[i] = scn.nextInt ();
     }
     int limit = (int) Math.pow (2, arr.length); //calculating the number of subsets
     for (int i = 0; i < limit; i++)
     {
-    
-    String set = "";
-    
-    int temp = i;
-    
-    for (int j = arr.length - 1; j > = 0; --j)
-    {
+      
+      String set = "";
+      
+      int temp = i;
+      
+      for (int j = arr.length - 1; j >= 0; --j)
+      {
+        
         int rem = temp % 2;
         temp = temp / 2;
-        if (rem == 0) 
+        if (rem == 0)   
         {
-        set = "-    " + set;
+          set = "- " + set;
         }
         else
         {
-        
-        set = arr[j] + "    " + set;
+          
+          set = arr[j] + " " + set;
         }
+      }
+      System.out.println (set);
+      
     }
-    System.out.println (set);
-    
-    }
-}
+  }
 }
