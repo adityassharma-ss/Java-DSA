af7474f7065c93707ac73153eae00208fecee48a
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-07-25 03:17:27 +0530

    Add files via upload

---
+++ Recursion/Decreasing.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Decreasing {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    printDecreasing(n);
+  }
+
+  public static void printDecreasing(int n) {
+    //Base case
+    if (n == 0)
+      return;
+
+    //Self Work
+    System.out.println(n);
+
+    //Recursive call
+    printDecreasing(n - 1);
+  }
+}
