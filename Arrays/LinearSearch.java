dd66d3f5fc1dc6c60ae95d50620bd7317e981005
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-07-28 13:57:11 +0530

    Add files via upload

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LinearSearch{
+    public static void main(String[] args){
+        Scanner io = new Scanner(System.in);
+        int n = io.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = io.nextInt();
+        }
+        int target = io.nextInt();
+
+        int ans = LinearSearch(a, target);
+        System.out.println("Target" + target + "is at index: " + ans);
+
+    }
+    static int LinearSearch(int[] a,int target){
+        if(a.length==0){
+            return -1;
+        }
+        for(int i=0;i<a.length;i++){
+            int element = a[i];
+            if(element == target){
+                return i;
+            }
+        }
+        return -1;
+    
+    }
+    
+}
