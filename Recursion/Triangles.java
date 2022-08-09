fd4576a931900d86b4922b4035a904de1f42cad7
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-08-09 23:07:01 +0530

    Create Triangles.java

---
+++ Recursion/Triangles.java
@@ -0,0 +1,31 @@
+public class Triangle {
+    public static void main(String[] args) {
+        triangle(4,0);
+        triangle2(4,0);
+    }
+    static void triangle(int r, int c){
+        if(r==0){
+            return;
+        }
+        if(c==r){
+            System.out.println();
+            triangle(r-1,0);
+        }
+        else{
+            System.out.print("* ");
+            triangle(r,c+1);
+        }
+    }
+    static void triangle2(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            triangle2(r, c+1);
+            System.out.print("*");
+        } else {
+            triangle2(r-1, 0);
+            System.out.println();
+        }
+    }
+}
