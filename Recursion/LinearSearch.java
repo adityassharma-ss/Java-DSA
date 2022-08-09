9564744efbc2e2abddf40c73cdddf9a2f72c6987
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-08-09 22:22:32 +0530

    Update LinearSearch.java

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -1,10 +1,18 @@
+import java.util.*;
+
 public class LinearSearch {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 4, 4 , 5, 6, 7, 8, 9, 10};
         System.out.println(linearSearch(arr, 5, 0));
         System.out.println(linearSearch2(arr, 5, 0));
         System.out.println(linearSearch3(arr, 5, arr.length-1));
+        findAllIndex(arr, 4, 0);
+        System.out.println(list);
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> ans = findAllIndex(arr, 4, 0, list);
+        System.out.println(ans);
+//        System.out.println(list);
         
     }
 
@@ -36,5 +44,28 @@ public class LinearSearch {
             return linearSearch3(arr, target, index - 1);
         }
     }
+
+    // for Multiple Occurrences
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
     
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, list);
+    }
 }
+
