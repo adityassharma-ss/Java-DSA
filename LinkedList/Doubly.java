c5de4c7d820ce2455144d580369a4b8a25b8d400
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-08-17 00:30:41 +0530

    Add files via upload

---
+++ LinkedList/Doubly.java
@@ -0,0 +1,106 @@
+
+public class Doubly {
+    Node head;
+    Node tail;
+    int size;
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public Node find(int value) {
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void insert(int after, int value) {
+        Node p = find(after);
+
+        if (p == null) {
+            System.out.println("does not exist");
+            return;
+        }
+
+        Node node = new Node(value);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if (node.next != null) {
+            node.next.prev = node;
+        }
+    }
+
+    public void display() {
+        Node node = head;
+        Node last = null;
+        while (node != null) {
+            System.out.print(node.value + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+
+        System.out.println("Print in reverse");
+        while (last != null) {
+            System.out.print(last.value + " -> ");
+            last = last.prev;
+        }
+        System.out.println("START");
+    }
+
+    public void insertLast(int val) {
+        Node node = new Node(val);
+        Node last = head;
+
+        node.next = null;
+
+        if (head == null) {
+            node.prev = null;
+            head = node;
+            return;
+        }
+
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = node;
+        node.prev = last;
+    }
+
+
+
+
+
+
+    private class Node{
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        
+    }
+    
+}
